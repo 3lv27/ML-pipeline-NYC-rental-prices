46c698119bcb297e17295c25930e9b5ab673daab
Elvin Gomez <elvin.gomez@inari.io>
2021-11-07 21:16:08 +0100

    drop rows in the dataset that are not in the proper geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -30,6 +30,10 @@ def go(args):
 
     df.to_csv(args.output_artifact, index=False)
 
+    # drop rows in the dataset that are not in the proper geolocation
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Upload to W&B
     logger.info("Upload output artifact %s of type %s", args.output_artifact, args.output_type)
     artifact = wandb.Artifact(
